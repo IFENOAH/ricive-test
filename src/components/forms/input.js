c67f352560e868b2bd3278fe0cb8159fb2dece96
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-11-09 12:48:41 +0100

    input outline fix

--- src/components/forms/input.js
+++ src/components/forms/input.js
@@ -4,7 +4,7 @@ const Input = ({leftIcon, padding, placeholder, bgColor}) => {
   return (
     <div className={`flex items-center space-x-2 border-2 border-ricive-stroke w-full px-2 bg-ricive-${bgColor} ${padding && padding} h-3/4 border rounded-lg`}>
         {leftIcon}
-        <input type='text'className={` border-none border-transparent focus:border-transparent focus:ring-0 text-ricive-darkgray text-sm w-full bg-ricive-${bgColor}`} placeholder= {placeholder} />
+        <input type='text'className={` outline-none border-none border-transparent focus:border-transparent focus:ring-0 text-ricive-darkgray text-sm w-full bg-ricive-${bgColor}`} placeholder= {placeholder} />
     </div>
   )
 }
